71dbafe1f647a7640f9e361a409635d43d304ac9
leusonmario <leusonmario@gmail.com>
2020-01-29 18:23:22 +0100

    'Trigger build #b540e44b'

--- src/main/java/org/dynjs/runtime/builtins/types/AbstractBuiltinNativeError.java
+++ src/main/java/org/dynjs/runtime/builtins/types/AbstractBuiltinNativeError.java
@@ -1,5 +1,4 @@
 package org.dynjs.runtime.builtins.types;
-
 import org.dynjs.runtime.DynObject;
 import org.dynjs.runtime.ExecutionContext;
 import org.dynjs.runtime.GlobalObject;
@@ -7,61 +6,51 @@ import org.dynjs.runtime.JSObject;
 import org.dynjs.runtime.PropertyDescriptor;
 import org.dynjs.runtime.StackGetter;
 import org.dynjs.runtime.Types;
-
 public class AbstractBuiltinNativeError extends AbstractBuiltinType {
-
-    private String name;
-
-    public AbstractBuiltinNativeError(GlobalObject globalObject, final String name) {
-        super(globalObject, "message");
-
-        final DynObject proto = new DynObject(globalObject);
-        this.defineReadOnlyProperty(globalObject, "prototype", proto);
-        this.name = name;
+  private String name;
+  public AbstractBuiltinNativeError(  GlobalObject globalObject,  final String name){
+    super(globalObject,"message");
+    final DynObject proto=new DynObject(globalObject);
+    this.defineReadOnlyProperty(globalObject,"prototype",proto);
+    this.name=name;
+  }
+  @Override public void initialize(  GlobalObject globalObject,  JSObject proto){
+    proto.setClassName("Error");
+    proto.defineNonEnumerableProperty(globalObject,"constructor",this);
+    proto.put(null,"name",this.name,false);
+    proto.put(null,"message","",false);
+    proto.setPrototype(globalObject.getPrototypeFor("Error"));
+  }
+  @Override public Object call(  final ExecutionContext context,  Object self,  final Object... args){
+    JSObject o=null;
+    if (self == Types.UNDEFINED) {
+      String errorType=(String)((JSObject)get(context,"prototype")).get(context,"name");
+      o=context.createError(errorType,args[0] == Types.UNDEFINED ? null : Types.toString(context,args[0]));
     }
-
-    @Override
-    public void initialize(GlobalObject globalObject, JSObject proto) {
-        proto.setClassName("Error");
-        proto.defineNonEnumerableProperty(globalObject, "constructor", this);
-        proto.put(null, "name", this.name, false);
-        proto.put(null, "message", "", false);
-        proto.setPrototype(globalObject.getPrototypeFor("Error"));
+ else {
+      o=(JSObject)self;
     }
-
-    @Override
-    public Object call(final ExecutionContext context, Object self, final Object... args) {
-        JSObject o = null;
-
-        if (self == Types.UNDEFINED) {
-            String errorType = (String) ((JSObject) get(context, "prototype")).get(context, "name");
-            o = context.createError(errorType, args[0] == Types.UNDEFINED ? null : Types.toString( context, args[0]) );
-        } else {
-            o = (JSObject) self;
+    if (args[0] != Types.UNDEFINED) {
+      o.defineOwnProperty(context,"message",new PropertyDescriptor(){
+{
+          set("Value",Types.toString(context,args[0]));
         }
-
-        if (args[0] != Types.UNDEFINED) {
-            o.defineOwnProperty(context, "message", new PropertyDescriptor() {
-                {
-                    set("Value", Types.toString( context, args[0]) );
-                }
-            }, false);
-        }
-
-        o.defineOwnProperty(context, "stack", new PropertyDescriptor() {
-            {
-                set("Get", new StackGetter(context));
-            }
-        }, false);
-
-        return o;
+      }
+,false);
     }
-
-    @Override
-    public JSObject createNewObject(ExecutionContext context) {
-        DynObject o = new DynObject(context.getGlobalObject());
-        o.setClassName("Error");
-        return o;
+    o.defineOwnProperty(context,"stack",new PropertyDescriptor(){
+{
+        set("Get",new StackGetter(context));
+      }
     }
-
+,false);
+    return o;
+  }
+  @Override public JSObject createNewObject(  ExecutionContext context){
+    DynObject o=new DynObject(context.getGlobalObject());
+    o.setClassName("Error");
+    return o;
+  }
+  public AbstractBuiltinNativeError(){
+  }
 }
